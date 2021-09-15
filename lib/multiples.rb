466f4f793d7da3f82e95203341d0379bc6b6d85b
Austin Rhoads <austinrrhoads@ymail.com>
2021-09-15 13:55:24 -0500

    commit tree 194d082fc8b6d027de7257e2f843c85b9e4f0ba3

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+
+def collect_multiples(nums)
+    array = Array(1..nums-1)
+    multiples = []
+    array.each do |num|
+        if num%3 == 0 || num%5 == 0
+            multiples.push(num)
+        end
+    end
+    multiples
+end
+
+collect_multiples(10)
+
+def sum_multiples(limit)
+  multiples =  collect_multiples(limit)
+ sum = multiples.inject(0){|sum,x| sum + x }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+require_relative './multiples.rb'
+class Multiples
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        self.limit = limit
+    end
+
+    def collect_multiples
+        array = Array(1..self.limit-1)
+        multiples = []
+        array.each do |num|
+            if num%3 == 0 || num%5 == 0
+                multiples.push(num)
+            end
+        end
+        multiples
+    end
+
+    def sum_multiples
+        multiples =  self.collect_multiples
+        sum = multiples.inject(0){|sum,x| sum + x }
+    end
+
+
+
+end
